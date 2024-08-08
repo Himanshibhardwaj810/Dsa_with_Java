d5e9f633d86dcc1dc67f7991b2587432d6835722
Himanshi <bhardwaj.himanshi2468@gmail.com>
2024-08-08 21:36:41 +0530

    leetcode Questions practised

---
+++ leetcode/src/leetcode/RBS_duplicated_values.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+public class RBS_duplicated_values {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- leetcode/src/leetcode/rotated_binary_search.java
+++ leetcode/src/leetcode/rotated_binary_search.java
@@ -4,8 +4,59 @@ public class rotated_binary_search {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr= {4,5,6,7,0,1,2};
+		//we use different logic for this rotated binary search for peak element because in mountain element the array though inc first then dec but element are sorted
+		//while in case of rotated binary search the element are first decreasing then inc
+		int[] arr= {3,5,1};
+		int target=3;
 		System.out.println(findpivot(arr));
+		System.out.println(search(arr, target));
+
+	}
+	static int search(int[] nums,int target) {
+		//if pivot element doesn't exist
+		int pivot=findpivot(nums);
+		if(pivot==-1) {
+			//just do normal binary search
+			return binarysearch(nums,target,0,nums.length-1);
+			
+		}
+		//now if pivot element is found
+		if(nums[pivot]==target) {
+			return pivot;
+		}
+		//as numbber after pivot are shorter then target
+		else if(target>=nums[0]) {
+			System.out.println(target+"cob");
+			System.out.println(nums[0]+"0th");
+			System.out.println(pivot+"p");
+			return binarysearch(nums,target,0,pivot-1);
+			
+		}else {
+			
+			return binarysearch(nums,target,pivot+1,nums.length-1);
+		}
+	}
+	static int binarysearch(int[] arr, int target,int start,int end) {
+		System.out.println(start);
+		System.out.println(end);
+	
+
+
+		while (start <= end) {//here equal to is because at the end only one index will be left
+			int mid = start + (end - start) / 2;
+			System.out.println(mid+"");
+			if (target > arr[mid]) {
+				start = mid + 1; //rhs
+			} else if (target < arr[mid]) {
+				System.out.println(mid+"jhv");
+				end = mid - 1;//lhs
+			} else {
+				System.out.println(mid+"");
+				return mid;
+			}
+		}
+		//When target is not found
+		return -1;
 
 	}
 
@@ -24,7 +75,7 @@ public class rotated_binary_search {
 				return mid;
 			}
 			if (mid > start && arr[mid] < arr[mid - 1]) {
-				return arr[mid - 1];
+				return mid - 1;
 			}
 			// al the elements after mid are shorter then start so ignore as we have to
 			// check the greatest element
