b42a049438777ffed3c69af26848d1f215ab4075
Himanshi <bhardwaj.himanshi2468@gmail.com>
2024-08-20 20:35:37 +0530

    OOPS concept

--- OOPS/src/oops/OOPS_concept.java
+++ OOPS/src/oops/OOPS_concept.java
@@ -10,17 +10,25 @@ public class OOPS_concept {
 		Student kunal;//here declaring the reference variable of object of datatype student
 //		System.out.println(kunal); 
 		kunal=new Student();//initialize
+		Student hima=new Student(15,"hima",90);
+		System.out.println(hima.name);
+		System.out.println(hima.rollno);
+		System.out.println(hima.marks);
+		//kunal
+		System.out.println(kunal.rollno);
+		System.out.println(kunal.name);
+		//method calling
+		kunal.greeting();
 		
 		
 		//in a single line
 //		Student kunal =new Student();
 //		System.out.println(Arrays.toString(students));
 		
-		kunal.rollno=45;
-		kunal.name="kunal";
-		System.out.println(kunal.rollno);
-		System.out.println(kunal.name);
-		System.out.println(kunal.pass);
+//		kunal.rollno=45;
+//		kunal.name="kunal";
+//		
+
 
 	}
 
@@ -31,4 +39,21 @@ class Student{
 	String name;
 	Float marks;
 	boolean pass=true;
+	
+	//constructor 
+	Student(){
+	this.rollno=45;
+	this.name="kunal";
+	this.marks=90f;
+	}
+	Student(int roll,String naam,float marks){
+		this.rollno=roll;
+		this.name=naam;
+		this.marks=marks;
+		}
+	//method
+	void greeting() {
+		System.out.println("hello"+name);
+	}
+	
 }
