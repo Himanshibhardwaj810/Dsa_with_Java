555d683c3fb824ad0da6dd470e108fe64187df51
Himanshi <bhardwaj.himanshi2468@gmail.com>
2024-12-10 10:05:58 +0530

    leetcode prob

--- leetcode/src/leetcode/remove_element.java
+++ leetcode/src/leetcode/remove_element.java
@@ -5,25 +5,21 @@ public class remove_element {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] nums= {3,2,2,3 };
-		int val = 3;
+		int val = 3  ;
 		System.out.println(removeElement(nums,val));
 
 	}
 	public static int removeElement(int[] nums, int val) {
-		int[] nums1=new int[5];
 		int j=0;
 		for(int i=0;i<nums.length;i++) {
-			System.out.println(nums[i]);
-//			if(nums[i]==val) {
-//				i++;
-//			}else {
-//			nums1[j]=nums[i];
-//			j++;
-//			}
+			if(nums[i]!=val) {
+				nums[j]=nums[i];
+				j++;
+			}
 		}
-		System.out.println(Arrays.toString(nums1));
-		int k=nums1.length;
-		return k;
+		System.out.println(Arrays.toString(nums));
+		;
+		return j;
 	
 	}
 
